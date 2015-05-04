5dd4d279e1249c7d11a6cfde0ecc294696f142fd
yeto <yujro.eto@gmail.com>
2015-05-04 12:28:23 +0900

    initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AnalyticsApp::Application.config.secret_key_base = '7319ef56701a4a2e22889e96cfd877c1b978fb7a98edbc9c43f439176ca8681ad0b6c4ecb126baa8d4a60dcb59dcb47d10b3ef069f8093abf19adff7549b3784'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AnalyticsApp::Application.config.session_store :cookie_store, key: '_analytics_app_session'
